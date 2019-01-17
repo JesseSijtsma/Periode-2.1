60dc0271b18df2af07e6d6c268ccd2be6ff04891
Jesse <34576204+JesseSijtsma@users.noreply.github.com>
2019-01-17 14:08:08 +0100

    Add files via upload

--- JS-animatie/js/script.js
+++ JS-animatie/js/script.js
@@ -1,8 +1,28 @@
 const btn = document.getElementById('btn');
-let flat = document.querySelector('.patch')
+let bars = document.getElementsByClassName('bar');
 
-const slide = () => {
-  flat.classList.toggle('patch--slide-out')
+
+const slide = ()=>{
+  bars[0].classList.toggle('bar--toggle');
+  bars[1].classList.toggle('bar--toggle');
+  bars[2].classList.toggle('bar--toggle');
+  bars[3].classList.toggle('bar--toggle');
+  bars[4].classList.toggle('bar--toggle');
 }
 
 btn.addEventListener('click', slide);
+
+
+var i = 0;
+var txt = 'Welcome.';
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("header").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+document.body.onload = typeWriter();
